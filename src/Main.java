d8dbf0380bf8c1504e2909731519e51e892b1b40
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-29 13:26:11 +0300

    https://www.patika.dev/tr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("enter array's lengt");
+        int n=scanner.nextInt();
+        int[] list=new int[n];
+        for (int i=0;i<list.length;i++){
+            System.out.println("enter "+(i+1)+"th number");
+            list[i]=scanner.nextInt();
+
+        }
+        Arrays.sort(list);
+        System.out.println(Arrays.toString(list));
+
     }
 }
